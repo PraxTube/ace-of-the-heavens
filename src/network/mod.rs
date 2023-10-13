eb708cf604a60cc5c2cebe70ec886cefeec340d7
PraxTube <lukinator28@gmail.com>
2023-10-13 12:19:38 +0200

    restructure!: Remove network.rs and move logic into network/mod.rs

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,7 +1,25 @@
 pub mod ggrs_config;
-pub mod network;
 pub mod session;
 pub mod socket;
 
 pub use ggrs_config::GgrsConfig;
-pub use network::AceNetworkPlugin;
+
+use bevy::prelude::*;
+
+use crate::GameState;
+use session::{start_matchbox_socket, wait_for_players, wait_for_seed};
+
+pub struct AceNetworkPlugin;
+
+impl Plugin for AceNetworkPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            (
+                wait_for_players.run_if(in_state(GameState::Matchmaking)),
+                wait_for_seed.run_if(in_state(GameState::Connecting)),
+            ),
+        )
+        .add_systems(OnEnter(GameState::Matchmaking), start_matchbox_socket);
+    }
+}
