f9f4a1ef369aa303c99affcb1970ccac229f4e26
PraxTube <lukinator28@gmail.com>
2023-10-26 19:47:02 +0200

    feat!: Improve the session stats UI

--- src/network/session_stats.rs
+++ src/network/session_stats.rs
@@ -3,36 +3,15 @@ use bevy_ggrs::{ggrs::NetworkStats, *};
 
 use super::{ggrs_config::PLAYER_COUNT, GgrsConfig};
 
-#[derive(Resource, Default)]
+#[derive(Resource)]
 pub struct SessionStats {
-    /// The length of the queue containing UDP packets which have not yet been acknowledged by the end client.
-    /// The length of the send queue is a rough indication of the quality of the connection.
-    /// The longer the send queue, the higher the round-trip time between the
-    /// clients. The send queue will also be longer than usual during high packet loss situations.
-    pub send_queue_len: usize,
-    /// The roundtrip packet transmission time as calculated by GGRS.
-    pub ping: u128,
-    /// The estimated bandwidth used between the two clients, in kilobits per second.
-    pub kbps_sent: usize,
-
-    /// The number of frames GGRS calculates that the local client is behind the remote client at this instant in time.
-    /// For example, if at this instant the current game client is running frame 1002 and the remote game client is running frame 1009,
-    /// this value will mostly likely roughly equal 7.
-    pub local_frames_behind: i32,
-    /// The same as [`local_frames_behind`], but calculated from the perspective of the remote player.
-    ///
-    /// [`local_frames_behind`]: #structfield.local_frames_behind
-    pub remote_frames_behind: i32,
+    pub network_stats: Vec<NetworkStats>,
 }
 
-impl From<NetworkStats> for SessionStats {
-    fn from(network_stats: NetworkStats) -> Self {
+impl Default for SessionStats {
+    fn default() -> Self {
         Self {
-            send_queue_len: network_stats.send_queue_len,
-            ping: network_stats.ping,
-            kbps_sent: network_stats.kbps_sent,
-            local_frames_behind: network_stats.local_frames_behind,
-            remote_frames_behind: network_stats.remote_frames_behind,
+            network_stats: vec![NetworkStats::default(); PLAYER_COUNT],
         }
     }
 }
@@ -45,8 +24,8 @@ pub fn update_session_stats(
         Session::P2P(s) => {
             for i in 0..PLAYER_COUNT {
                 match s.network_stats(i) {
-                    Ok(stats) => *session_stats = SessionStats::from(stats),
-                    Err(_) => {}
+                    Ok(stats) => session_stats.network_stats[i] = stats,
+                    Err(_) => session_stats.network_stats[i] = NetworkStats::default(),
                 }
             }
         }
--- src/ui/session_stats_screen.rs
+++ src/ui/session_stats_screen.rs
@@ -1,49 +1,185 @@
 use bevy::prelude::*;
 
+use crate::network::ggrs_config::PLAYER_COUNT;
 use crate::network::session_stats::SessionStats;
 use crate::GameAssets;
+
+const ROW_GAP: f32 = 5.0;
+const COLUMN_GAP: f32 = 0.0;
 
 #[derive(Component)]
 pub struct StatsText;
 
-fn spawn_text(commands: &mut Commands, font: Handle<Font>) -> Entity {
+#[derive(Component)]
+pub struct SessionStatsRow {
+    pub handle: usize,
+    pub sql: Entity,
+    pub ping: Entity,
+    pub kbps: Entity,
+    pub lframes: Entity,
+    pub rframes: Entity,
+}
+
+fn spawn_text(commands: &mut Commands, font: Handle<Font>, content: Option<&str>) -> Entity {
+    let text = match content {
+        Some(t) => t.to_string(),
+        None => String::new(),
+    };
+    let sections = vec![TextSection {
+        value: text,
+        style: TextStyle {
+            font,
+            font_size: 30.0,
+            color: Color::WHITE,
+        },
+    }];
+
     commands
         .spawn((
             StatsText,
-            TextBundle::from_section(
-                String::new(),
-                TextStyle {
-                    font,
-                    font_size: 30.0,
-                    color: Color::WHITE,
+            TextBundle {
+                text: Text {
+                    sections,
+                    alignment: TextAlignment::Center,
+                    ..default()
                 },
-            )
-            .with_text_alignment(TextAlignment::Center),
+                style: Style {
+                    width: Val::Px(150.0),
+                    ..default()
+                },
+                ..default()
+            },
         ))
         .id()
 }
 
-pub fn spawn_stats_text(mut commands: Commands, assets: Res<GameAssets>, stats: Res<SessionStats>) {
+fn spawn_session_header_row(commands: &mut Commands, font: Handle<Font>) -> Entity {
+    let player = spawn_text(commands, font.clone(), Some("ID"));
+    let sql = spawn_text(commands, font.clone(), Some("SQL"));
+    let ping = spawn_text(commands, font.clone(), Some("PING"));
+    let kbps = spawn_text(commands, font.clone(), Some("Kb/s"));
+    let lframes = spawn_text(commands, font.clone(), Some("LF"));
+    let rframes = spawn_text(commands, font.clone(), Some("RF"));
+
+    let root_node = commands
+        .spawn((NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Row,
+                column_gap: Val::Vh(COLUMN_GAP),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..default()
+        },))
+        .id();
+
+    commands
+        .entity(root_node)
+        .push_children(&[player, sql, ping, kbps, lframes, rframes]);
+    root_node
+}
+
+fn spawn_session_text_row(commands: &mut Commands, font: Handle<Font>, handle: usize) -> Entity {
+    let player = spawn_text(commands, font.clone(), Some(&handle.to_string()));
+    let sql = spawn_text(commands, font.clone(), None);
+    let ping = spawn_text(commands, font.clone(), None);
+    let kbps = spawn_text(commands, font.clone(), None);
+    let lframes = spawn_text(commands, font.clone(), None);
+    let rframes = spawn_text(commands, font.clone(), None);
+
+    let root_node = commands
+        .spawn((
+            SessionStatsRow {
+                handle,
+                sql,
+                ping,
+                kbps,
+                lframes,
+                rframes,
+            },
+            NodeBundle {
+                style: Style {
+                    flex_direction: FlexDirection::Row,
+                    column_gap: Val::Vh(COLUMN_GAP),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                ..default()
+            },
+        ))
+        .id();
+
+    commands
+        .entity(root_node)
+        .push_children(&[player, sql, ping, kbps, lframes, rframes]);
+    root_node
+}
+
+pub fn spawn_stats_text(mut commands: Commands, assets: Res<GameAssets>) {
     let font = assets.font.clone();
     let root_node = commands
         .spawn(NodeBundle {
             style: Style {
                 width: Val::Vw(100.0),
                 height: Val::Vh(100.0),
+                flex_direction: FlexDirection::Column,
+                row_gap: Val::Vh(ROW_GAP),
                 justify_content: JustifyContent::Center,
                 align_items: AlignItems::Center,
-                position_type: PositionType::Absolute,
                 ..default()
             },
             ..default()
         })
         .id();
 
-    let text = spawn_text(&mut commands, font);
+    let text = spawn_session_header_row(&mut commands, font.clone());
     commands.entity(root_node).push_children(&[text]);
+
+    for handle in 0..PLAYER_COUNT {
+        let text = spawn_session_text_row(&mut commands, font.clone(), handle);
+        commands.entity(root_node).push_children(&[text]);
+    }
 }
 
-pub fn update_stats_text(mut text: Query<&mut Text, With<StatsText>>, stats: Res<SessionStats>) {
-    let mut text = text.single_mut();
-    text.sections[0].value = format!("Ping {}", stats.ping);
+// let mut text_str = String::new();
+// // SQL, PING, Kb/s, LFrames, RFrames
+// for (i, n_stats) in stats.network_stats.iter().enumerate() {
+//     text_str += &format!("PLAYER {}, PING: {}\n", i, n_stats.ping);
+// }
+// text.sections[0].value = text_str;
+
+pub fn update_stats_text(
+    stats: Res<SessionStats>,
+    row_stats: Query<&SessionStatsRow>,
+    mut stats_text: Query<&mut Text, With<StatsText>>,
+) {
+    for session_stats_row in &row_stats {
+        if let Ok(mut text) = stats_text.get_mut(session_stats_row.sql) {
+            text.sections[0].value = stats.network_stats[session_stats_row.handle]
+                .send_queue_len
+                .to_string();
+        }
+        if let Ok(mut text) = stats_text.get_mut(session_stats_row.ping) {
+            text.sections[0].value = stats.network_stats[session_stats_row.handle]
+                .ping
+                .to_string();
+        }
+        if let Ok(mut text) = stats_text.get_mut(session_stats_row.kbps) {
+            text.sections[0].value = stats.network_stats[session_stats_row.handle]
+                .kbps_sent
+                .to_string();
+        }
+        if let Ok(mut text) = stats_text.get_mut(session_stats_row.lframes) {
+            text.sections[0].value = stats.network_stats[session_stats_row.handle]
+                .local_frames_behind
+                .to_string();
+        }
+        if let Ok(mut text) = stats_text.get_mut(session_stats_row.rframes) {
+            text.sections[0].value = stats.network_stats[session_stats_row.handle]
+                .remote_frames_behind
+                .to_string();
+        }
+    }
 }
