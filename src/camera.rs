0fc9173e184f0c8d875c893c14d542285556d787
PraxTube <lukinator28@gmail.com>
2023-10-29 15:13:36 +0100

    feat! Use noise instead of RNG for camera shake
    Adding support to change the camera shake settings from the console
    would be useful for dynamically testing (and even allowing players to
    test camera settings), but for now this will do.
    
    Closes #50.

--- src/camera.rs
+++ src/camera.rs
@@ -1,12 +1,14 @@
-use rand::prelude::*;
+use chrono::Utc;
 
 use bevy::{prelude::*, render::camera::ScalingMode};
+use noisy_bevy::simplex_noise_2d_seeded;
 
 use crate::GameState;
 
 const CAMERA_POSITION: Vec3 = Vec3::new(0.0, 50.0, 0.0);
-const TRANSLATION_SHAKE_STRENGTH: f32 = 100.0;
-const ROTATION_SHAKE_STRENGTH: f32 = 8.0;
+const NOISE_STRENGTH: f32 = 10.0;
+const TRANSLATION_SHAKE_STRENGTH: f32 = 50.0;
+const ROTATION_SHAKE_STRENGTH: f32 = 2.0;
 
 pub struct AceCameraPlugin;
 
@@ -24,11 +26,26 @@ impl Plugin for AceCameraPlugin {
 #[derive(Resource, Default, Reflect)]
 pub struct CameraShake {
     trauma: f32,
+    seed: f32,
 }
 
 impl CameraShake {
     pub fn add_trauma(&mut self, trauma: f32) {
-        self.trauma = (self.trauma + trauma).clamp(0.0, 1.0);
+        if self.trauma == 0.0 {
+            self.seed = (Utc::now().timestamp_millis() & 0xFFFF) as f32;
+        }
+        self.trauma = (self.trauma + trauma.abs()).min(1.0);
+    }
+
+    fn reduce_trauma(&mut self, delta: f32) {
+        self.trauma = (self.trauma - delta.abs()).max(0.0)
+    }
+
+    fn noise_value(&mut self, stack: u32) -> f32 {
+        simplex_noise_2d_seeded(
+            Vec2::new(self.trauma * NOISE_STRENGTH, 0.0),
+            self.seed + stack as f32,
+        )
     }
 }
 
@@ -45,18 +62,16 @@ fn camera_shake(
     time: Res<Time>,
 ) {
     let mut transform = camera.single_mut();
-    info!("{:?}", transform.translation);
 
-    let mut rng = rand::thread_rng();
-    let translation_offset = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0)
+    let translation_offset = Vec3::new(shake.noise_value(0), shake.noise_value(1), 0.0)
         * shake.trauma.powi(2)
         * TRANSLATION_SHAKE_STRENGTH;
     let rotation_offset = Quat::from_rotation_z(
-        (rng.gen_range(-1.0..1.0) * shake.trauma.powi(2) * ROTATION_SHAKE_STRENGTH).to_radians(),
+        (shake.noise_value(2) * shake.trauma.powi(2) * ROTATION_SHAKE_STRENGTH).to_radians(),
     );
 
     transform.translation = CAMERA_POSITION + translation_offset;
     transform.rotation = Quat::IDENTITY + rotation_offset;
 
-    shake.trauma = (shake.trauma - time.delta_seconds()).max(0.0);
+    shake.reduce_trauma(time.delta_seconds());
 }
