5e542abb640694eb30d96c1e5c79c54451025807
PraxTube <lukinator28@gmail.com>
2024-01-17 14:35:30 +0100

    fix!: Remove desync trigger and change player inputs

--- src/misc/debug.rs
+++ src/misc/debug.rs
@@ -3,9 +3,6 @@ use std::hash::{Hash, Hasher};
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
-use crate::player::shooting::bullet::BulletTimer;
-use crate::player::Player;
-use crate::world::Seeds;
 use crate::{GameState, RollbackState};
 
 #[derive(Reflect, Component, Default)]
@@ -15,7 +12,6 @@ pub struct DebugVec3(Vec3);
 #[reflect(Hash)]
 pub struct DebugQuat(Quat);
 
-/// We will store the world position of the mouse cursor here.
 #[derive(Resource, Default)]
 pub struct MouseWorldCoords(Vec2);
 
@@ -29,7 +25,6 @@ impl Plugin for AceDebugPlugin {
                 debug_state_main_menu.run_if(
                     in_state(GameState::MainMenu).and_then(not(in_state(RollbackState::Setup))),
                 ),
-                trigger_desync.run_if(in_state(GameState::InRollbackGame)),
                 print_mouse_transform.run_if(in_state(GameState::InRollbackGame)),
             ),
         )
@@ -82,43 +77,23 @@ fn debug_state_main_menu() {
     error!("the rollbackstate is not in setup. This is most likely caused by rollingback nextstate calls");
 }
 
-pub fn trigger_desync(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut bullet_timers: Query<&mut BulletTimer, With<Player>>,
-) {
-    if !keyboard_input.pressed(KeyCode::ShiftLeft) {
-        return;
-    }
-
-    for mut bullet_timer in &mut bullet_timers {
-        bullet_timer.timer.reset();
-    }
-}
-
 pub fn setup_mouse_tracking(mut commands: Commands) {
     commands.init_resource::<MouseWorldCoords>();
 }
 
 pub fn print_mouse_transform(
     mut mycoords: ResMut<MouseWorldCoords>,
-    // query to get the window (so we can read the current cursor position)
     q_window: Query<&Window, With<PrimaryWindow>>,
-    // query to get camera transform
     q_camera: Query<(&Camera, &GlobalTransform), With<Camera2d>>,
     buttons: Res<Input<MouseButton>>,
-    seeds: Res<Seeds>,
 ) {
     if !buttons.just_pressed(MouseButton::Left) {
         return;
     }
 
-    info!("{:?}", seeds.0);
-
     let (camera, camera_transform) = q_camera.single();
     let window = q_window.single();
 
-    // check if the cursor is inside the window and get its position
-    // then, ask bevy to convert into world coordinates, and truncate to discard Z
     if let Some(world_position) = window
         .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
